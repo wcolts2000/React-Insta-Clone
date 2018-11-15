f4de759fc0bf63589ca2bb722066fb21892872e7
wcolts2000 <wcolts2000@gmail.com>
2018-11-15 15:41:26 -0800

    Updates

---
+++ precache-manifest.53262230609ff0ec64c485dec09e668a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a1d6d5df149028b5c7e5da3c9fab4a7",
+    "url": "/React-Insta-Clone/static/media/instagramScript.4a1d6d5d.svg"
+  },
+  {
+    "revision": "8bea3be130bc96ea5570",
+    "url": "/React-Insta-Clone/static/js/runtime~main.8bea3be1.js"
+  },
+  {
+    "revision": "7b095741445563f66212",
+    "url": "/React-Insta-Clone/static/js/main.7b095741.chunk.js"
+  },
+  {
+    "revision": "577544a73143013b44b0",
+    "url": "/React-Insta-Clone/static/js/1.577544a7.chunk.js"
+  },
+  {
+    "revision": "7b095741445563f66212",
+    "url": "/React-Insta-Clone/static/css/main.16ef3f52.chunk.css"
+  },
+  {
+    "revision": "2bb72f622983d27c9c1680fe57cf970a",
+    "url": "/React-Insta-Clone/index.html"
+  }
+];
